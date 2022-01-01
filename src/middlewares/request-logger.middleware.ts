08afe4d4c6e0967528bc3badd15acb4f6d418dec
Adhityan K V <me@adhityan.com>
2022-01-01 11:49:34 +0400

    Initial commit

---
+++ src/middlewares/request-logger.middleware.ts
@@ -0,0 +1,16 @@
+import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers';
+import { Logger } from '@adhityan/gc-logger';
+
+@Middleware({ type: 'before' })
+export class RequestLoggerMiddleWare implements ExpressMiddlewareInterface {
+    // eslint-disable-next-line class-methods-use-this
+    public async use(
+        request: Express.Request,
+        response: Express.Response,
+        next: () => Promise<unknown>,
+    ): Promise<unknown> {
+        response.startTime = new Date();
+        Logger.info(`Request ${request.url}`);
+        return next();
+    }
+}
